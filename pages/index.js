457c9d78f4eea703248fade7fd0ce4461fcbe656
emeliepetersson <emepet1204@skola.goteborg.se>
2020-09-01 12:24:12 +0200

    add styled components

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,92 @@
-import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+
 import Nav from "../components/Nav";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Container>
       <Nav />
-      <main className={styles.main}>
-        <h1 className={styles.title}>Gundla Gårdscafé</h1>
+      <main className="main">
+        <h1 className="title">Gundla Gårdscafé</h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+        <p className="description">
+          Get started by editing <code className="code">pages/index.js</code>
         </p>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .main {
+    padding: 5rem 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .title {
+      margin: 0;
+      line-height: 1.15;
+      font-size: 4rem;
+    }
+
+    .title,
+    .description {
+      text-align: center;
+    }
+
+    .description {
+      line-height: 1.5;
+      font-size: 1.5rem;
+
+      .code {
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 0.75rem;
+        font-size: 1.1rem;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      }
+    }
+  }
+
+  footer {
+    width: 100%;
+    height: 100px;
+    border-top: 1px solid #eaeaea;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .logo {
+        margin-left: 0.5rem;
+        height: 1em;
+      }
+    }
+  }
+`;
