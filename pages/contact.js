58fb67c002e98ac15234e897e44a75add686a202
Andreas Pandzic <andreas.pandzic@hotmail.com>
2020-09-02 13:04:05 +0200

    add subpages

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import colors from "../config/colors";
+import Layout from "../components/Layout";
+
+function Contact() {
+    return (
+      <Layout>
+        <Container>
+          <h1>Contact</h1>
+        </Container>
+      </Layout>
+    )
+  }
+  
+  export default Contact
+
+  const Container = styled.div``;
