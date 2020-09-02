2f72f510df6617e026ada710e93ab7d79aca819b
Andreas Pandzic <andreas.pandzic@hotmail.com>
2020-09-02 13:04:05 +0200

    add subpages

---
+++ pages/event.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import colors from "../config/colors";
+import Layout from "../components/Layout";
+
+function Event() {
+    return (
+      <Layout>
+        <Container>
+          <h1>Event</h1>
+        </Container>
+      </Layout>
+    )
+  }
+  
+  export default Event
+
+  const Container = styled.div``;
