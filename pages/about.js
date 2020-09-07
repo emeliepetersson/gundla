e16a6a779338cd4383238c846605a86fb1c54d1c
emeliepetersson <emepet1204@skola.goteborg.se>
2020-09-07 17:41:26 +0200

    add Hero component to about page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 import colors from "../config/colors";
 import Layout from "../components/Layout";
+import Hero from "../components/Hero";
 
 function About() {
-    return (
-      <Layout>
-        <Container>
-          <h1>About</h1>
-        </Container>
-      </Layout>
-    )
-  }
-  
-  export default About
+  return (
+    <Layout>
+      <Container>
+        <Hero imageUrl="/images/hero-test-portrait.jpg" />
+        <h1>About</h1>
+      </Container>
+    </Layout>
+  );
+}
 
-  const Container = styled.div``;
+export default About;
+
+const Container = styled.div``;
