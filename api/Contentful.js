2439987ef6d3ad7a8ecc8ccbfb89705e431fed6e
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-09-08 13:31:50 +0200

    install contentful
    and make a client

---
+++ api/Contentful.js
@@ -0,0 +1,8 @@
+const contentful = require("./node_modules/contentful");
+
+const client = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export default client;
