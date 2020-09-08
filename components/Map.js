894e7c30a17b6f8ad34b87ea193ea2b75e6626f9
Marcusiscode <marcusaugustssonn@gmail.com>
2020-09-08 13:15:31 +0200

    inforbadge and more

--- components/Map.js
+++ components/Map.js
@@ -6,7 +6,7 @@ const zoom = 6435;
 const mapSrc = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d${zoom}!2d12.028094864162664!3d57.68635801874599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff3a5d0157c63%3A0xdd291fa6017dc04a!2zR3VuZGxhIEfDpXJkc2NhZsOp!5e0!3m2!1ssv!2sse!4v1599459390820!5m2!1ssv!2sse`
 
 
-function Map() {
+const Map =()=> {
   
     return (
         <MapContainer>
--- config/colors.js
+++ config/colors.js
@@ -6,4 +6,5 @@ export default {
   medium: "#6e6969",
   light: "#f8f4f4",
   dark: "#0c0c0c",
+  infoGreen:"#4E633A",
 };
