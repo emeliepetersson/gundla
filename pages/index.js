8af1c6dd3edaa7b5400e86c5046832facab891f9
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-09-10 17:41:28 +0200

    add all content from contentful

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,10 @@ import Hero from "../components/Hero";
 import ContactInfo from "../components/ContactInfo";
 import SocialMedia from "../components/SocialMedia";
 import Post from "../components/Post";
+import Button from "../components/Button";
 import { fetchEntries } from "../pages/api/Contentful";
 
 export default function Home({ contactInfo, landingPage }) {
-  console.log(landingPage);
-
   return (
     <Container>
       <Hero
@@ -26,10 +25,33 @@ export default function Home({ contactInfo, landingPage }) {
         text="Här kan du avnjuta härlig mat eller fika, eller bara ha en skön stund."
       />
 
+      <Post
+        title={landingPage.title2}
+        text="Här kan du avnjuta härlig mat eller fika, eller bara ha en skön stund."
+      />
+
       <ContactInfo
         adress={contactInfo.adress}
         postcode={contactInfo.postcode}
         openingHours={contactInfo.openingHours}
+      />
+
+      <Button>Hitta hit</Button>
+
+      <Post
+        buttonText="Se alla evenemang"
+        imageUrl={landingPage.image3.fields.file.url}
+        altText={landingPage.image3.fields.description}
+        title={landingPage.title3}
+        text="Här kan du avnjuta härlig mat eller fika, eller bara ha en skön stund."
+      />
+
+      <Post
+        buttonText="Till catering"
+        imageUrl={landingPage.image4.fields.file.url}
+        altText={landingPage.image4.fields.description}
+        title={landingPage.title4}
+        text="Här kan du avnjuta härlig mat eller fika, eller bara ha en skön stund."
       />
 
       <SocialMedia
@@ -43,11 +65,11 @@ export default function Home({ contactInfo, landingPage }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetchEntries("visitingInfo");
-  const response = await res.map((i) => {
+  const contactInfoRes = await fetchEntries("visitingInfo");
+  const contactInfoResponse = await contactInfoRes.map((i) => {
     return i.fields;
   });
-  const contactInfo = response[0];
+  const contactInfo = contactInfoResponse[0];
 
   const landingPageRes = await fetchEntries("landingpage");
   const landingPageResponse = await landingPageRes.map((i) => {
