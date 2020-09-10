e2e408b63116409b6c99ff9d3ee2c090865707ff
emeliepetersson <emepet1204@skola.goteborg.se>
2020-09-10 14:00:28 +0200

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -31,7 +31,7 @@ Button.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   color: PropTypes.string,
-  bg: PropTypes.String,
+  bg: PropTypes.string,
 };
 
 export default Button;
