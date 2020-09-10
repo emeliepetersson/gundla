60d6da250c823c92217368fa4e54f928eefeb2ba
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-09-10 14:56:35 +0200

    add post component
    and remove some code

--- pages/index.js
+++ pages/index.js
@@ -17,41 +17,26 @@ export default function Home({ contactInfo }) {
         text="Genuin enkelhet
           i ranchanda"
       />
-      <div className="main">
-        <h1 className="title">Gundla Gårdscafé</h1>
+      <Post
+        buttonText="Till vår meny"
+        imageUrl="/images/hero-test.jpg"
+        altText="example picture"
+        title="Välkommen till Gundla Gårdscafé"
+        text="Här kan du avnjuta härlig mat eller fika, eller bara ha en skön stund."
+      />
 
-        <Post
-          buttonText="Till vår meny"
-          imageUrl="/images/hero-test.jpg"
-          altText="example picture"
-          title="Välkommen till Gundla Gårdscafé"
-          text="Här kan du avnjuta härlig mat eller fika, eller bara ha en skön stund."
-        />
+      <ContactInfo
+        adress={contactInfo.adress}
+        postcode={contactInfo.postcode}
+        openingHours={contactInfo.openingHours}
+      />
 
-        <ContactInfo
-          adress={contactInfo.adress}
-          postcode={contactInfo.postcode}
-          openingHours={contactInfo.openingHours}
-        />
-
-        <SocialMedia
-          icons={[
-            { url: "/icons/instagram-black.png", altText: "Instagram icon" },
-          ]}
-          text="Följ oss på Instagram!"
-        />
-      </div>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <SocialMedia
+        icons={[
+          { url: "/icons/instagram-black.png", altText: "Instagram icon" },
+        ]}
+        text="Följ oss på Instagram!"
+      />
     </Container>
   );
 }
@@ -70,46 +55,8 @@ export const getStaticProps = async () => {
 };
 
 const Container = styled.div`
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  .main {
-    padding: 5rem 0;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    .title {
-      margin: 0;
-      line-height: 1.15;
-      font-size: 4rem;
-      color: ${colors.primary};
-      text-align: center;
-    }
-  }
-
-  footer {
-    width: 100%;
-    height: 100px;
-    border-top: 1px solid ${colors.light};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    a {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      .logo {
-        margin-left: 0.5rem;
-        height: 1em;
-      }
-    }
-  }
 `;
