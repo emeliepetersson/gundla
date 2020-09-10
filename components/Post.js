e6f2c32abc51f80f945935f0d6347f404fd8d61e
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-09-10 14:56:07 +0200

    Update Post.js
    Add styling, prop types and if-statements

--- components/Post.js
+++ components/Post.js
@@ -6,10 +6,16 @@ import Button from "./Button";
 const ContactInfo = ({ altText, buttonText, imageUrl, title, text }) => {
   return (
     <Container>
-      <img src={imageUrl} alt={altText} />
-      <h2>{title}</h2>
-      <p>{text}</p>
-      <Button>{buttonText}</Button>
+      {imageUrl && (
+        <div className="image-container">
+          <img src={imageUrl} alt={altText} />
+        </div>
+      )}
+      <div className="content">
+        <h2>{title}</h2>
+        {text && <p>{text}</p>}
+        {buttonText && <Button>{buttonText}</Button>}
+      </div>
     </Container>
   );
 };
@@ -20,18 +26,45 @@ const Container = styled.div`
   justify-content: center;
   flex-direction: column;
   text-align: center;
-  width: 100%;
-  /* padding-top: 100%;  */
-  /**https://www.w3schools.com/howto/howto_css_aspect_ratio.asp */
-  position: relative;
   overflow: hidden;
+  width: 100%;
+  background-color: cornflowerblue;
+  padding-bottom: 80px;
 
-  img {
+  .image-container {
+    position: relative;
     width: 100%;
+    padding-top: 100%;
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .content {
+    padding: 48px 37px 0;
+
+    p {
+      margin: 32px 0 20px;
+    }
+
+    button {
+      margin-top: 40px;
+    }
   }
 `;
 
 ContactInfo.propTypes = {
+  altText: PropTypes.string,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string,
+  text: PropTypes.string,
   buttonText: PropTypes.string,
 };
 
