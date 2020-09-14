7659b46936645e8df366958cec036e04553c127b
Andreas Pandzic <andreas.pandzic@hotmail.com>
2020-09-14 14:43:33 +0200

    getInitialProps in app.js for passing footer data and footer component mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,58 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import ContactInfo from "../components/ContactInfo";
+import SocialMedia from "../components/SocialMedia";
+import colors from "../config/colors";
 
 
+const Footer =({footerData})=>{
+  return(
+    <Container>
+        <h2>Gundla Gårdscafé</h2>
+        <ContactInfo
+          adress={footerData.adress}
+          postcode={footerData.postcode}
+          openingHours={footerData.openingHours}
+          email={footerData.email}
+          phonenumber ={footerData.phoneNumber}
+        />
+        <SocialMedia className="footer-icon-container"
+          icons={[
+            { url: "/icons/instagram-white.png", altText: "Instagram icon" },
+            { url: "/icons/facebook-white.png", altText: "Facebook icon" },
+          ]}
+        />
+    </Container>
+    );
+};
 
-const Footer =()=>{
-    return(
-    <footer>
-      just some temp footer text
-    </footer>
-    )
-}
+Footer.propTypes = {
+  footerData: PropTypes.object,
+};
+
 export default Footer;
+
+const Container = styled.footer`
+*{
+  color: ${colors.white};
+}
+background-color: #0a0a0a;
+padding-top: 96px;
+padding-bottom: 176px;
+
+h2 {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+}
+
+h3 {
+  margin-top: 32px;
+}
+
+.footer-icon-container {
+  margin-top: 40px;
+}
+`;
