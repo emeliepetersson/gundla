3b70ad13adc258229f2205eaa0b936e3af73c77c
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-09-23 14:43:01 +0200

    update icons

--- components/Layout.js
+++ components/Layout.js
@@ -18,13 +18,13 @@ const Layout = ({ children, title, footerData }) => (
 
       <link rel="manifest" href="/manifest.json" />
       <link
-        href="/favicon-16x16.png"
+        href="/favicon16x16.ico"
         rel="icon"
         type="image/png"
         sizes="16x16"
       />
       <link
-        href="/favicon-32x32.png"
+        href="/favicon32x32.ico"
         rel="icon"
         type="image/png"
         sizes="32x32"
@@ -33,7 +33,7 @@ const Layout = ({ children, title, footerData }) => (
         href="https://fonts.googleapis.com/css2?family=Noto+Sans&family=Noto+Serif&display=swap"
         rel="stylesheet"
       />
-      <link rel="apple-touch-icon" href="/apple-icon.png" />
+      <link rel="apple-touch-icon" href="/logo.png" />
       <meta name="theme-color" content="#317EFB" />
     </Head>
     <Nav />
